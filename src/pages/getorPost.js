879aa0c8a6e5703ab763d759d23901986987d2c7
Sravya-M <sravya.mengavaram@gmail.com>
2020-04-30 15:18:37 +0530

    Basic get and display

--- src/pages/getorPost.js
+++ src/pages/getorPost.js
@@ -4,36 +4,20 @@ import axios from 'axios';
 import  {Navbar,Nav,Container,Row,Jumbotron,Col} from 'react-bootstrap'
 
 class getorPost extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
+    this.handlePost = this.handlePost.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
+    this.handleGet = this.handleGet.bind(this);
   }
 
   handleSubmit(event) {
-    if(this.state.value== 'c')
-    {
-      this.props.history.push('/');
-    }
-    else if (this.state.value=='p') {
-      this.props.history.push('/');
-    }
     event.preventDefault();
   }
-  
-    onSubmit = () => {
-          this.props.history.push('/');
-      
-   }
 
-   handleClick() { this.props.history.push('/post'); console.log('this is:', this); } 
-   handleClickp() { this.props.history.push('/get'); console.log('this is:', this); }
+   handlePost() { this.props.history.push('/post'); console.log('this is:', this); } 
+   handleGet() { this.props.history.push('/get'); console.log('this is:', this); }
 
     render() {
       const marginTop={
@@ -56,19 +40,16 @@ class getorPost extends Component {
 
                 <div className="FormCenter" >
                     <form onSubmit={this.handleSubmit} className="FormFields">
+                      
                       <div className="FormField">
                         <label className="FormField__Label" htmlFor="name">CHOOSE WHAT YOU WANNA DO?</label>
-              
-                    
                       </div>
         
                       <div className="FormField">
-                  
-                   <input  className="FormField__Button mr-20"  type="submit" value="POST"  onClick={() => this.handleClick()} /> 
-                   <input  className="FormField__Button mr-20"  type="submit" value="GET"  onClick={() => this.handleClicky()} /> 
-                       
-              
+                        <input  className="FormField__Button mr-20"  type="submit" value="POST"  onClick={() => this.handlePost()} /> 
+                        <input  className="FormField__Button mr-20"  type="submit" value="GET"  onClick={() => this.handleGet()} /> 
                       </div>
+
                     </form>
                   </div>
               </Col>

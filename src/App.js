e0c23f78717d2c84078b7cfda6b116f4ec43c0c9
sac <sachinbagalakoti08@gmail.com>
2020-04-30 11:23:10 +0530

    changing App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Welcome from './pages/Welcome'
 import subjects from './pages/subjects'
 import semester from './pages/semester'
 import Dropdown from './pages/Dropdown'
-<<<<<<< HEAD
 import getorPost from './pages/getorPost'
 import post from './pages/post';
-=======
+//import get from './pages/get';
 import login from './pages/login'
 import register from './pages/register'
->>>>>>> 11b4a5ea8fdbfe90e21f7160032c338464daed65
 import NavigationBar from './components/NavigationBar'
 import  {Navbar,Nav,Container,Row,Jumbotron,Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,43 +25,20 @@ class App extends Component {
 
     return (
       <Router basename="/react-auth-ui/">
-<<<<<<< HEAD
         <NavigationBar/>
-
-          <Route exact path='/' component={Welcome}>
-          </Route>
-          <Route exact path='/semester' component={semester}>
-          </Route>
-           <Route exact path='/subjects' component={subjects}>
-          </Route>
-          <Route exact path='/Dropdown' component={Dropdown}>
-          </Route>
-          <Route exact path='/getorPost' component={getorPost}>
-          </Route>
-          <Route exact path='/post' component={post}>
-          </Route>
-          </Router>
-=======
-        <NavigationBar history={this.props.history}/>
-          <Container >
-            <Row>
-              <Col lg={12} style={marginTop}>
-                <Route exact path='/welcome' component={Welcome}/>
-                <Route exact path='/register' component={register}/>
-                <Route exact path='/' component={login}>
-                </Route>
-                <Route exact path='/semester' component={semester}>
-                </Route>
-                <Route exact path='/subjects' component={subjects}>
-                </Route>
-                <Route exact path='/Dropdown' component={Dropdown}>
-                </Route>
-
-              </Col>
-            </Row>
-          </Container>
+        <Col lg={12} style={marginTop}>
+          
+          <Route exact path='/' component={login}/>
+          <Route exact path='/register' component={register}/>
+          <Route exact path='/welcome' component={Welcome}/>
+          <Route exact path='/semester' component={semester}/>
+          <Route exact path='/subjects' component={subjects}/>
+          <Route exact path='/Dropdown' component={Dropdown}/>
+          <Route exact path='/getorPost' component={getorPost}/>
+          <Route exact path='/post' component={post}/>
+          
+        </Col>
       </Router>
->>>>>>> 11b4a5ea8fdbfe90e21f7160032c338464daed65
     )
   }
 }
